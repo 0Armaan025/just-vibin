b3a9b35cab4eb0392234f60f130766b5aa626a0d
Armaan <armaan33000@gmail.com>
2024-06-25 19:50:11 +0530

    first

---
+++ src/app/landing-page/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const LandingPage = (props: Props) => {
+  return <>
+        
+    </>;
+};
+
+export default LandingPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import LandingPage from "./landing-page/LandingPage";
+
+export default function Home() {
+  return <LandingPage />;
+}
